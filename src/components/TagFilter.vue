7487abc0f56ab44c54f694ca8833a713a3e278bc
yasukei <yasu_kei@icloud.com>
2025-07-27 22:21:06 +0900

    added graduated members on TagFilter

--- src/components/TagFilter.vue
+++ src/components/TagFilter.vue
@@ -1,31 +1,52 @@
 <script setup lang="ts">
-import { batch2Names, batch3Names, batch4Names, batch5Names } from '@/data/members.ts'
+import { getXbatchNames } from '@/data/members.ts'
 import { getChannelTitles } from '@/data/channels.ts'
 import TagList from '@/components/TagList.vue'
+import { useSelectedTagsStore } from '@/stores/selectedTags.ts'
+
+const { clear } = useSelectedTagsStore()
 
 const batches = [
   {
     title: '2期生',
-    memberNames: batch2Names,
+    memberNames: getXbatchNames(2, false),
   },
   {
     title: '3期生',
-    memberNames: batch3Names,
+    memberNames: getXbatchNames(3, false),
   },
   {
     title: '4期生',
-    memberNames: batch4Names,
+    memberNames: getXbatchNames(4, false),
   },
   {
     title: '5期生',
-    memberNames: batch5Names,
+    memberNames: getXbatchNames(5, false),
+  },
+]
+const batchesGraduated = [
+  {
+    title: '1期生',
+    memberNames: getXbatchNames(1, true),
+  },
+  {
+    title: '2期生',
+    memberNames: getXbatchNames(2, true),
+  },
+  // {
+  //   title: '3期生',
+  //   memberNames: getXbatchNames(3, true),
+  // },
+  {
+    title: '4期生',
+    memberNames: getXbatchNames(4, true),
   },
 ]
 </script>
 
 <template>
   <div class="">
-    <h2 class="text-center">フィルタ</h2>
+    <h2 class="text-center">フィルタ<button class="clear-btn" @click="clear">（解除）</button></h2>
     <ul>
       <li>
         <h3>チャンネル</h3>
@@ -40,12 +61,25 @@ const batches = [
           </li>
         </ul>
       </li>
+      <li>
+        <h3>卒業メンバー</h3>
+        <ul>
+          <li v-for="batch in batchesGraduated" :key="batch.title">
+            <h4>{{ batch.title }}</h4>
+            <TagList :tags="batch.memberNames" :canClick="true" />
+          </li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
 
 <style lang="postcss" scoped>
 @reference "../assets/main.css";
+
+.clear-btn {
+  @apply text-sm;
+}
 
 h4 {
   @apply mt-2 ml-2;
--- src/data/members.ts
+++ src/data/members.ts
@@ -9,20 +9,12 @@ export interface Member {
 
 export const members: { [key: string]: Member } = membersJson
 
-function getXbatchMembers(batch: number): Member[] {
-  return Object.values(members).filter((member) => member.batch == batch)
+export function getXbatchMembers(batch: number, graduated: boolean): Member[] {
+  return Object.values(members).filter(
+    (member) => member.batch === batch && member.graduated === graduated,
+  )
 }
 
-function getXbatchNames(batch: number): string[] {
-  return getXbatchMembers(batch).map((member) => member.name)
+export function getXbatchNames(batch: number, graduated: boolean): string[] {
+  return getXbatchMembers(batch, graduated).map((member) => member.name)
 }
-
-export const batch2Members = getXbatchMembers(2)
-export const batch3Members = getXbatchMembers(3)
-export const batch4Members = getXbatchMembers(4)
-export const batch5Members = getXbatchMembers(5)
-
-export const batch2Names = getXbatchNames(2)
-export const batch3Names = getXbatchNames(3)
-export const batch4Names = getXbatchNames(4)
-export const batch5Names = getXbatchNames(5)
