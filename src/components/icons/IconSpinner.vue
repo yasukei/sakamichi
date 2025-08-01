b62385fecd8ec1517a899588d7cdd64551590d0f
yasukei <yasu_kei@icloud.com>
2025-08-01 14:35:18 +0900

    use primary-color on IconSpinner

--- src/components/icons/IconSpinner.vue
+++ src/components/icons/IconSpinner.vue
@@ -2,7 +2,7 @@
 
 <template>
   <svg
-    class="animate-spin h-5 w-5 text-blue-500"
+    class="animate-spin h-5 w-5 text-primary-color"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
