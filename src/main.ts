d9655696947c877f6391a31e77df72585b16a3fe
yasukei <yasu_kei@icloud.com>
2025-08-10 16:05:36 +0900

    added google analytics

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,24 @@ import router from './router'
 
 const app = createApp(App)
 
+const GOOGLE_ANALYTICS_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID
+if (GOOGLE_ANALYTICS_ID) {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`
+  document.head.appendChild(script)
+
+  const script2 = document.createElement('script')
+  script2.text = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${GOOGLE_ANALYTICS_ID}');
+  `
+  document.head.appendChild(script2)
+}
+
 app.use(createPinia())
 app.use(router)
 
